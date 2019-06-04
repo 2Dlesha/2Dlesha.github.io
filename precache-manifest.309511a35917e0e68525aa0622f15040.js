4a9c8887b64112f1a6fea2e47397a5ab84eae8c4
Ilya <44382262+2Dlesha@users.noreply.github.com>
2019-06-04 20:13:52 +0300

    Add files via upload

---
+++ precache-manifest.309511a35917e0e68525aa0622f15040.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb00160e9e3f967c686a4b3d695d7005",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afc7cc2c7773df569a03",
+    "url": "/static/css/main.572b8214.chunk.css"
+  },
+  {
+    "revision": "603fffc0b6fc0d606132",
+    "url": "/static/js/2.0ed7d092.chunk.js"
+  },
+  {
+    "revision": "afc7cc2c7773df569a03",
+    "url": "/static/js/main.3c2b1243.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
